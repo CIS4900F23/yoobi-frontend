3e03346ed42e4b9e8339978395a46c8597ffd936
loganhagen <loganhagen@gmail.com>
2023-10-30 13:52:25 -0400

    use env file for hostname and ports

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
 
-const postsProxyTarget = `http://posts:8080`;
-const yoobiProxyTarget = `http://yoobi-bot:3001`;
+const { POSTS_HOST, POSTS_PORT, YOOBI_HOST, YOOBI_PORT } = process.env;
+const postsProxyTarget = `${POSTS_HOST}:${POSTS_PORT}`;
+const yoobiProxyTarget = `${YOOBI_HOST}:${YOOBI_PORT}`;
 
 const config: UserConfig = {
 	plugins: [sveltekit()],
